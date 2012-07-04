75e944befa15a4afad0c33dc3d472a61f45200bf
Ben McCall <ben@iogo.com>
2012-07-04 17:56:39 -0500

    Update flag limit to 2

--- stldataportal/settings.py
+++ stldataportal/settings.py
@@ -217,7 +217,7 @@ LOGIN_REDIRECT_URL = "/"
 
 # Tags (See: https://github.com/liberation/django-flag#readme)
 FLAG_ALLOW_COMMENTS = True
-FLAG_LIMIT_SAME_OBJECT_FOR_USER = 1
+FLAG_LIMIT_SAME_OBJECT_FOR_USER = 2
 FLAG_LIMIT_FOR_OBJECT = 0
 FLAG_MODELS = (
     'maps.map',
